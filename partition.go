c96f9835abaf7c67d0010ce60de5477515ea074b
Evan Huus <eapache@gmail.com>
2015-09-13 11:54:33 -0400

    Age out old messages to avoid OOM

--- partition.go
+++ partition.go
@@ -8,8 +8,11 @@ import (
 
 type Partition struct {
 	l     sync.Mutex
+	base  int64
 	cache []*sarama.Message
 }
+
+const maxCache = 1024
 
 func NewPartition() *Partition {
 	return &Partition{}
@@ -18,7 +21,7 @@ func NewPartition() *Partition {
 func (p *Partition) Produce(set *sarama.MessageSet) int64 {
 	p.l.Lock()
 	defer p.l.Unlock()
-	nextOffset := int64(len(p.cache))
+	firstOffset := p.nextOffset()
 	for _, msg := range set.Messages {
 		if msg.Msg.Set != nil {
 			p.Produce(msg.Msg.Set)
@@ -26,13 +29,26 @@ func (p *Partition) Produce(set *sarama.MessageSet) int64 {
 			p.cache = append(p.cache, msg.Msg)
 		}
 	}
-	return nextOffset
+	if len(p.cache) > maxCache {
+		trim := len(p.cache) - maxCache
+		p.base += int64(trim)
+		p.cache = p.cache[trim:]
+	}
+	return firstOffset
 }
 
 func (p *Partition) Offsets() (oldest, newest int64) {
 	p.l.Lock()
 	defer p.l.Unlock()
-	return 0, int64(len(p.cache))
+	return p.oldestOffset(), p.nextOffset()
+}
+
+func (p *Partition) oldestOffset() int64 {
+	return p.base
+}
+
+func (p *Partition) nextOffset() int64 {
+	return p.base + int64(len(p.cache))
 }
 
 func (p *Partition) Fetch(req *sarama.FetchRequestBlock) *sarama.FetchResponseBlock {
@@ -41,14 +57,14 @@ func (p *Partition) Fetch(req *sarama.FetchRequestBlock) *sarama.FetchResponseBl
 
 	response := &sarama.FetchResponseBlock{
 		Err:                 sarama.ErrNoError,
-		HighWaterMarkOffset: int64(len(p.cache)),
+		HighWaterMarkOffset: p.nextOffset(),
 	}
 
-	if req.FetchOffset > int64(len(p.cache)) {
+	if req.FetchOffset > p.nextOffset() {
 		response.Err = sarama.ErrOffsetOutOfRange
-	} else if req.FetchOffset == int64(len(p.cache)) {
+	} else if req.FetchOffset == p.nextOffset() {
 	} else {
-		for i, msg := range p.cache[req.FetchOffset:] {
+		for i, msg := range p.cache[req.FetchOffset-p.base:] {
 			response.MsgSet.Messages = append(response.MsgSet.Messages, &sarama.MessageBlock{
 				Offset: req.FetchOffset + int64(i),
 				Msg:    msg,
